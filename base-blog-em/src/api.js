b459a39e54efaa305878d7dd461b3dbd9d679afb
hyeon17 <janghyeon8725@naver.com>
2024-04-02 22:52:32 +0900

    Feat: 로딩 상태, 에러 상태 처리

--- base-blog-em/src/api.js
+++ base-blog-em/src/api.js
@@ -1,29 +1,26 @@
 export async function fetchPosts(pageNum = 1) {
-  const response = await fetch(
-    `https://jsonplaceholder.typicode.com/posts?_limit=10&_page=${pageNum}`
-  );
-  return response.json();
+	try {
+		const response = await fetch(`https://jsonplaceholder.typicode.com/posts?_limit=10&_page=${pageNum}`);
+		return response.json();
+	} catch (error) {
+		throw new Error(error);
+	}
 }
 
 export async function fetchComments(postId) {
-  const response = await fetch(
-    `https://jsonplaceholder.typicode.com/comments?postId=${postId}`
-  );
-  return response.json();
+	const response = await fetch(`https://jsonplaceholder.typicode.com/comments?postId=${postId}`);
+	return response.json();
 }
 
 export async function deletePost(postId) {
-  const response = await fetch(
-    `https://jsonplaceholder.typicode.com/posts/${postId}`,
-    { method: "DELETE" }
-  );
-  return response.json();
+	const response = await fetch(`https://jsonplaceholder.typicode.com/posts/${postId}`, { method: 'DELETE' });
+	return response.json();
 }
 
 export async function updatePost(postId) {
-  const response = await fetch(
-    `https://jsonplaceholder.typicode.com/posts/${postId}`,
-    { method: "PATCH", data: { title: "REACT QUERY FOREVER!!!!" } }
-  );
-  return response.json();
+	const response = await fetch(`https://jsonplaceholder.typicode.com/posts/${postId}`, {
+		method: 'PATCH',
+		data: { title: 'REACT QUERY FOREVER!!!!' },
+	});
+	return response.json();
 }
